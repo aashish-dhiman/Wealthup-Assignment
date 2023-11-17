e132f21ee091317f50aef6d6000e3ccfb00db923
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-11-17 18:30:38 +0530

    use type module

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors =require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
+import express, { json } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-dotenv.config();
+config();
 
 const PORT = process.env.PORT || 8080;
-app.use(express.json());
+app.use(json());
 
 const connectDB = async () => {
     try {
@@ -21,9 +21,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log(
-            `Connected to MongoDB database ${mongoose.connection.host}`
-        );
+        console.log(`Connected to MongoDB database ${mongoose.connection.host}`);
     } catch (error) {
         console.log(`Error in MongoDB connection- ${error}`);
     }
